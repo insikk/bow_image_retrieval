f4156bbe24ea53d3017cfcf17e50e549de0c8091
insikk <insik92@gmail.com>
2017-12-20 19:11:44 +0900

    adding minhash step

--- utils/oxf5k_feature_reader.py
+++ utils/oxf5k_feature_reader.py
@@ -1,1 +1,23 @@
-import os
+import numpy as np
+
+def feature_reader(feature_bin_path="./data/feature/feat_oxc1_hesaff_sift.bin"):
+    """
+    This method reads official oxf5k descriptor.
+    
+    binary file contains 128byte sift descriptor. It is 128-d integer vector. 
+    """
+        
+    features = []    
+    # Read feature from bin file, and make tuple format. 
+    with open(feature_bin_path, "rb") as f:
+        # Read 128 byte        
+        raw_binary = f.read(128)
+        while len(raw_binary) == 128:
+            dt = np.dtype(np.uint8)
+            # dt = dt.newbyteorder('>')
+            descriptor = np.frombuffer(raw_binary, dtype=dt)
+            features.append(descriptor)
+            raw_binary = f.read(128)
+            
+    return features
+
